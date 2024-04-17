e3395dab1ea7850eb21d46a1014b20702495d6ba
addyfoxer <adarshku12345@gmail.com>
2024-04-17 15:45:49 +0530

    Sepo mig

--- Exchange/hardhat-tutorial/hardhat.config.js
+++ Exchange/hardhat-tutorial/hardhat.config.js
@@ -7,9 +7,9 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    goerli: {
-      url: QUICKNODE_HTTP_URL,
-      accounts: [PRIVATE_KEY],
+    sepolia: {
+      url: "https://cold-sparkling-sea.ethereum-sepolia.quiknode.pro/f9fe50b03782764a240afbfa523dd7ef35721b2c/",
+      accounts: ["af43d23484d33402801c67cdcddd7e0116ee81d6d5c5cdf8ac35d6da0dc3bf80"],
     },
   },
 };
--- Exchange/my-app/pages/Home.js
+++ Exchange/my-app/pages/Home.js
@@ -294,12 +294,7 @@ export default function Home() {
     const provider = await web3ModalRef.current.connect();
     const web3Provider = new providers.Web3Provider(provider);
 
-    // If user is not connected to the Goerli network, let them know and throw an error
-    const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 5) {
-      window.alert("Change the network to Goerli");
-      throw new Error("Change network to Goerli");
-    }
+    
 
     if (needSigner) {
       const signer = web3Provider.getSigner();
